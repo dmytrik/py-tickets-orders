b08304748d05034bb336671d3490baef099c45cb
dmytro <dmysamo@gmail.com>
2024-12-09 18:33:13 +0200

    add ticket validation

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -1,6 +1,16 @@
+from django.db import transaction
 from rest_framework import serializers
+from rest_framework.validators import UniqueTogetherValidator
 
-from cinema.models import Genre, Actor, CinemaHall, Movie, MovieSession
+from cinema.models import (
+    Genre,
+    Actor,
+    CinemaHall,
+    Movie,
+    MovieSession,
+    Ticket,
+    Order
+)
 
 
 class GenreSerializer(serializers.ModelSerializer):
@@ -78,3 +88,60 @@ class MovieSessionDetailSerializer(MovieSessionSerializer):
     class Meta:
         model = MovieSession
         fields = ("id", "show_time", "movie", "cinema_hall")
+
+
+class TicketSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Ticket
+        fields = ("id", "movie_session", "row", "seat")
+        validators = [
+            UniqueTogetherValidator(
+                queryset=Ticket.objects.all(),
+                fields=["movie_session", "row", "seat"]
+            )
+        ]
+
+    def validate(self, attrs):
+        Ticket.validate_seat(
+            attrs["row"],
+            attrs["seat"],
+            attrs["movie_session"].cinema_hall.rows,
+            attrs["movie_session"].cinema_hall.seats_in_row,
+            serializers.ValidationError
+        )
+        # if not (1 <= attrs["row"] <= attrs["movie_session"].cinema_hall.rows):
+        #     raise serializers.ValidationError(
+        #         {
+        #             "row": [
+        #                 f"row number must be in available range: (1, rows): "
+        #                 f"(1, {attrs["movie_session"].cinema_hall.rows})"
+        #             ]
+        #         }
+        #     )
+        # if not (1 <= attrs["seat"] <= attrs["movie_session"].cinema_hall.seats_in_row):
+        #     raise serializers.ValidationError(
+        #         {
+        #             "seat": [
+        #                 f"seat number must be in available range: "
+        #                 f"(1, seats_in_row): "
+        #                 f"(1, {attrs["movie_session"].cinema_hall.seats_in_row})"
+        #             ]
+        #         }
+        #     )
+
+class OrderSerializer(serializers.ModelSerializer):
+
+    tickets = TicketSerializer(many=True, read_only=False, allow_empty=False)
+
+    class Meta:
+        model = Order
+        fields = ("id", "created_at", "tickets")
+
+    def create(self, validated_data):
+        with transaction.atomic():
+            tickets_data = validated_data.pop("tickets")
+            order = Order.objects.create(**validated_data)
+            for ticket_data in tickets_data:
+                Ticket.objects.create(order=order, **ticket_data)
+            return order
